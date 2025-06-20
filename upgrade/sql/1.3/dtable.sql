7a39e9a59262a1e7914acc30af225911cac106fe
杨国璇 <ygx@MacBookPro.lan>
2025-06-20 15:13:36 +0800

    remove: update sql

--- upgrade/sql/1.3/dtable.sql
+++
@@ -1,3 +0,0 @@
-DROP TABLE IF EXISTS dtable_plugin;
-
-ALTER TABLE dtable_forms ADD COLUMN created_at DATETIME(6), ADD COLUMN submit_count INT(11) DEFAULT 0;
--- upgrade/sql/2.3/dtable.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE dtable_notification_rules ADD KEY `ctime_k3x7o2p4_key` (`ctime`), ADD KEY `last_trigger_time_b3n4m2l1_key` (`last_trigger_time`);
-
-ALTER TABLE dtable_snapshot DROP INDEX IF EXISTS `commit_id`;
-ALTER TABLE dtable_snapshot ADD CONSTRAINT `dtable_snapshot_dtable_uuid_commit_id_b16af249_uniq` UNIQUE (`dtable_uuid`, `commit_id`);
-
-ALTER TABLE dtable_external_apps ADD COLUMN visit_times int(11) NOT NULL DEFAULT 0 AFTER created_at;
-
-ALTER TABLE dtable_automation_rules ADD COLUMN trigger_count int(11) NOT NULL DEFAULT 0;
