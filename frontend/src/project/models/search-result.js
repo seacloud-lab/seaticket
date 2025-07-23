bd978d9120758ca354931d70509fd541d5d43d5c
‘JoinTyang’ <yangtong1009@163.com>
2025-07-23 16:42:52 +0800

    search in all types

--- frontend/src/project/models/search-result.js
+++ frontend/src/project/models/search-result.js
@@ -1,7 +1,7 @@
-class SearchResult {
+class SiteResult {
   constructor(object) {
     this.id = object._id || '';
-    this.site_id = object.site_id || '';
+    this.connection_id = object.connection_id || '';
     this.title = object.title || '';
     this.score = object.score || 0;
     this.url = object.url || '';
@@ -9,4 +9,32 @@ class SearchResult {
   }
 }
 
-export default SearchResult;
+class SeafileResult {
+  constructor(object) {
+    this.id = object._id || '';
+    this.connection_id = object.connection_id || '';
+    this.filename = object.filename || '';
+    this.path = object.path || 0;
+    this.repo_id = object.repo_id || '';
+    this.content = object.content || '';
+  }
+}
+
+class SeafileResult1 {
+  constructor(object, object2) {
+    this.site = object._id || '';
+    this.connection_id = object.connection_id || '';
+    this.filename = object.filename || '';
+    this.path = object.path || 0;
+    this.repo_id = object.repo_id || '';
+    this.content = object.content || '';
+  }
+}
+
+const SearchResults = (results) => {
+  const siteResults = results.site_list ? results.site_list.map(r => new SiteResult(r)) : [];
+  const seafileResults = results.seafile_list ? results.seafile_list.map(r => new SeafileResult(r)) : [];
+  return { 'site_list': siteResults, 'seafile_list': seafileResults };
+};
+
+export { SiteResult, SeafileResult, SearchResults };
