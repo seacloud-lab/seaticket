6824db64d8c2530504cb2cba2f62340f1336a023
SkywalkerSpace <dzmbbs@qq.com>
2025-08-18 17:03:04 +0800

    PROJECT_FILE_MAX_SIZE

--- seahub/project/apis.py
+++ seahub/project/apis.py
@@ -27,7 +27,7 @@ from seahub.project.utils import check_project_admin_permission, check_project_p
     add_init_crawl_site_task, add_index_seafile_task, get_project_related_users, encrypt_config, decrypt_config, \
     create_default_project_tags, gen_project_tags_dict, upload_file_to_tmp_dir, get_file_from_s3, \
     replace_file_url_in_content, upload_files_to_s3, delete_file_from_s3, gen_tmp_upload_file_path, add_github_issues_index_task
-from seahub.project.constants import ConnectionType, TICKET_STATUS, TICKET_TYPE
+from seahub.project.constants import ConnectionType, TICKET_STATUS, TICKET_TYPE, IMAGE_EXTS
 
 
 SEAQA_VERSION = getattr(settings, 'SEAQA_VERSION', 'Dev')
@@ -1423,8 +1423,11 @@ class ProjectUploadFileAPIView(APIView):
             error_msg = 'file not found.'
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
-        if file.size > 1024 * 1024 * settings.PROJECT_FILE_SIZE_MAX:
-            error_msg = 'file too large.'
+        if file.name.split('.')[-1] in IMAGE_EXTS and file.size > 1024 * 1024 * settings.PROJECT_IMAGE_MAX_SIZE:
+            error_msg = 'Image size cannot exceed %s Mb.' % settings.PROJECT_IMAGE_MAX_SIZE
+            return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+        if file.size > 1024 * 1024 * settings.PROJECT_FILE_MAX_SIZE:
+            error_msg = 'File size cannot exceed %s Mb.' % settings.PROJECT_FILE_MAX_SIZE
             return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
 
         # resource check
