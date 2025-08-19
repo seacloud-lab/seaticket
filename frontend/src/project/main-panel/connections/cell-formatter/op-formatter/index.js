2970c8d19683e1466825f92260f81a66dc498b8a
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2025-08-19 18:16:09 +0800

    update ui

--- frontend/src/project/main-panel/connections/cell-formatter/op-formatter/index.js
+++ frontend/src/project/main-panel/connections/cell-formatter/op-formatter/index.js
@@ -19,22 +19,30 @@ const OpFormatter = ({ onModify, onDelete, showStatus, onManualCrawl, row }) => 
       {onModify &&
         <IconButton className="bg-color-deep mr-1" title={gettext('Edit')} icon="rename" onClick={() => onModify(row)} />
       }
-      {onManualCrawl && isSite &&
-        (<IconButton className="bg-color-deep" title={gettext('Crawl now')} icon="search" onClick={() => onManualCrawl(row)} />)
-      }
       {onDelete &&
         <IconButton className="bg-color-deep mr-1" title={gettext('Delete')} icon="delete" onClick={() => onDelete(row)} />
       }
 
-      {showStatus &&
+      {(showStatus || onManualCrawl) &&
         <Dropdown isOpen={dropdownOpen} toggle={toggle} size="sm">
           <DropdownToggle className="bg-color-deep" tag="span">
             <IconButton className="bg-color-deep" icon="more" onClick={toggle} />
           </DropdownToggle>
           <DropdownMenu>
-            <DropdownItem onClick={() => showStatus(row)}>
-              {gettext('Status')}
-            </DropdownItem>
+            {showStatus &&
+              (
+                <DropdownItem onClick={() => showStatus(row)}>
+                  {gettext('Status')}
+                </DropdownItem>
+              )
+            }
+            {onManualCrawl && isSite &&
+              (
+                <DropdownItem onClick={() => onManualCrawl(row)}>
+                  {gettext('Sync now')}
+                </DropdownItem>
+              )
+            }
           </DropdownMenu>
         </Dropdown>
       }
