715988c93f3f2770195d78843b280b99170452b7
Michael An <2331806369@qq.com>
2025-08-19 16:22:29 +0800

    fix loading icon bug (#133)

--- frontend/src/project/models/index.js
+++ frontend/src/project/models/index.js
@@ -1,9 +1,7 @@
-import Connection from './connection';
 import SearchResult from './search-result';
 import ChatMessage from './chat-message';
 
 export {
-  Connection,
   SearchResult,
   ChatMessage,
 };
