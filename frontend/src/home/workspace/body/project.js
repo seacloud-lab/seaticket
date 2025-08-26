bc47c8c12c47bf9b47d4aee1f284fe29092057c6
Michael An <2331806369@qq.com>
2025-08-26 17:43:10 +0800

    Fix some UI issue (#155)
    * fix issue 02
    
    * fix issue 03
    
    * fix issue 04
    
    * optimise codes
    
    * fix warnings
    
    * fix issue 05
    
    * fix issue 07
    
    * fix issue 08
    
    * update css timestamp
    
    * fix npm audit

--- frontend/src/home/workspace/body/project.js
+++ frontend/src/home/workspace/body/project.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Icon, IconButton, ProjectIcon, toaster } from '../../../components';
+import { IconButton, ProjectIcon, toaster } from '../../../components';
 import { Utils, validateName } from '../../../utils/utils';
 import { ProjectSettingPopover } from '../../popover';
 import { PROJECT_BACKGROUND_COLOR_MAP, PROJECT_HOVER_COLOR_MAP, DEFAULT_COLOR } from '../constants';
@@ -164,7 +164,7 @@ class Project extends React.Component {
   };
 
   render() {
-    let { isOwner, isAdmin, project, className = '', style = {}, workspace } = this.props;
+    let { isOwner, isAdmin, project, className = '', style = {} } = this.props;
     let { name: newName, bgColor, icon, active, isMoreOperationPopoverShow } = this.state;
     let { workspace_id, id } = project;
     let projectHref = siteRoot + 'workspace/' + workspace_id + '/project/' + encodeURIComponent(project.name) + '/';
@@ -218,10 +218,7 @@ class Project extends React.Component {
         <div className="project-item-name" title={project.name}>
           {project.name}
         </div>
-        <div className="project-item-group text-truncate">
-          <Icon symbol="collaborator" className="project-workspace-icon" />
-          {workspace.name}
-        </div>
+        <div className="project-item-group"></div>
         {this.state.isShowSettings && (
           <ProjectSettingPopover
             target={`project-item-${id}`}
