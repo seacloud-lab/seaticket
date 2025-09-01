859d0ecb796bf852d478565f33385eb558662aee
杨国璇 <ygx@MacBookPro.lan>
2025-09-01 09:53:01 +0800

    feat: tags manage

--- frontend/src/sea-metadata/components/data-process-setter/index.js
+++ frontend/src/sea-metadata/components/data-process-setter/index.js
@@ -2,10 +2,12 @@ import FilterSetter from './filter-setter';
 import SortSetter from './sort-setter';
 import GroupbySetter from './groupby-setter';
 import HideColumnSetter from './hide-column-setter';
+import ManageSetter from './manage-setter';
 
 export {
   FilterSetter,
   SortSetter,
   GroupbySetter,
   HideColumnSetter,
+  ManageSetter,
 };
