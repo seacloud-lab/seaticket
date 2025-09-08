56c7ca76f1db8a043e613923ce064fcbb48797fb
yinjianfei <1358009667@com>
2025-09-08 09:49:08 +0800

    update forum detail

--- frontend/src/project/main-panel/connections/components/discourse-forums-details/index.js
+++ frontend/src/project/main-panel/connections/components/discourse-forums-details/index.js
@@ -5,7 +5,6 @@ import { gettext, mediaUrl } from '@/constants';
 import './index.css';
 
 const DiscourseForumsDetails = ({ rowDetails, onClose }) => {
-  console.log('rowDetails', rowDetails);
 
   return (
     <Modal className='sea-qa-discourse-forums-details-container' isOpen={true} toggle={onClose} style={{ minWidth: 800 }}>
@@ -18,11 +17,11 @@ const DiscourseForumsDetails = ({ rowDetails, onClose }) => {
               <div className='sea-qa-discourse-forums-reply-item-author-info-wrapper'>
                 <div className='sea-qa-discourse-forums-reply-item-author-info-left'>
                   <div className='sea-qa-discourse-forums-reply-item-author-avatar'>
-                    <img alt='' src='https://dev.seafile.com/seahub/image-view/avatars/c/0/c945fc16322c9a5ab68b0b6797d0a9/resized/256/394659692a460258b45a99f1424ea357.png'/>
+                    <img alt='' src={`${mediaUrl}avatars/default.png`}/>
                   </div>
                   <div className='sea-qa-discourse-forums-reply-item-author-name'>{detail.author}</div>
                 </div>
-                <div className='sea-qa-discourse-forums-reply-item-author-time'>2025-09-03 17:47:11</div>
+                <div className='sea-qa-discourse-forums-reply-item-author-time'>{detail.reply_time}</div>
               </div>
               <div className='sea-qa-discourse-forums-reply-item-content' dangerouslySetInnerHTML={{ __html: detail.content }}></div>
             </div>
