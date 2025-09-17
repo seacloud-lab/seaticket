09c36a2103d8bd3cbfc94fa055edc7be4ffebb30
yinjianfei <1358009667@com>
2025-09-17 11:37:37 +0800

    add option

--- frontend/src/project/main-panel/tickets/components/ticket-settings/rate-settings/index.js
+++ frontend/src/project/main-panel/tickets/components/ticket-settings/rate-settings/index.js
@@ -3,6 +3,8 @@ import { Label } from 'reactstrap';
 import classnames from 'classnames';
 import { gettext } from '@/constants';
 import { Option, OptionEditor } from '@/components';
+import CustomizePopover from '@/components/customize-popover';
+import { RATE_LIST } from './constants';
 import { useTypes } from '../../../hooks';
 
 import './index.css';
@@ -44,9 +46,9 @@ const RateSettings = ({
   return (
     <>
       <div className={classnames('sea-qa-project-ticket-settings-item', className)}>
-        <Label>{gettext('Type')}</Label>
-        <div className="ticket-types-formatter" onClick={openEditor} ref={editorRef}>
-          {typeOption ? (<Option option={typeOption} />) : (<div className="tip-default">{gettext('No type')}</div>)}
+        <Label>{gettext('priority')}</Label>
+        <div className='ticket-rate-formatter' onClick={openEditor} ref={editorRef}>
+          {typeOption ? (<Option option={typeOption} />) : (<div className="tip-default">{gettext('No priority')}</div>)}
         </div>
       </div>
       {!isReadonly && isShowEditor && (
@@ -60,6 +62,29 @@ const RateSettings = ({
           onChange={onTypeChange}
           onToggle={closeEditor}
         />
+        // <CustomizePopover
+        //   target={`rate-editor-${column.key}-${row._id}`}
+        //   className={classnames('sea-metadata-rate-editor-popover-container')}
+        //   hidePopover={() => setIsOpen(false)}
+        //   hidePopoverWithEsc={() => setIsOpen(false)}
+        //   modifiers={[
+        //     { name: 'preventOverflow', options: { boundary: document.body } },
+        //     { name: 'offset', options: { offset: [-6, 8] } }
+        //   ]}
+        // >
+        //   <div className="sea-metadata-rate-editor-popover">
+        //     {RATE_LIST.map((item, index) => (
+        //       <RateItem
+        //         key={index}
+        //         value={item.value}
+        //         hotKey={item.hotKey}
+        //         onClick={onChangeValue}
+        //         readOnly={false}
+        //         isSelected={item.value === value}
+        //       />
+        //     ))}
+        //   </div>
+        // </CustomizePopover>
       )}
     </>
   );
