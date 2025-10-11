293c843721aba6496b421ef2692962b2ad6c92dd
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2025-10-11 11:59:04 +0800

    fix: connection status

--- frontend/src/utils/type-detection.js
+++ frontend/src/utils/type-detection.js
@@ -4,3 +4,10 @@
  * @returns boolean
  */
 export const isNumber = (number) => (number || number === 0) && Object.prototype.toString.call(number) === '[object Number]';
+
+/**
+ * Check whether is object
+ * @param {object} object
+ * @returns boolean
+ */
+export const isObject = (object) => object && Object.prototype.toString.call(object) === '[object Object]';
