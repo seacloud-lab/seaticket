710ec32e0388acb3a736591d891d164e71215db1
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2025-10-25 17:01:04 +0800

    fix: seafile columns

--- seahub/project/constants.py
+++ seahub/project/constants.py
@@ -278,7 +278,7 @@ CONNECTION_DISPLAY_ALL_COLUMNS = {
     ConnectionType.GITHUB_ISSUE.value: ['_pk','title', 'author', 'state', 'state_reason', 'issue_type', 'labels', 'comments_count', 'closed_at', 'created_at', 'updated_at'],
     ConnectionType.DISCOURSE_FORUM.value: ['_pk', 'title', 'topic_id', 'views', 'bumped_at', 'created_at'],
     ConnectionType.SITE.value: ['_pk', 'url', 'title', 'last_modified'],
-    ConnectionType.SEAFILE.value: ['path', 'filename', 'mtime', 'updated_at'],
+    ConnectionType.SEAFILE.value: ['_pk', 'path', 'filename', 'mtime', 'updated_at'],
 }
 
 
