60d8b28113e500ef48cae61c792db0c606334c74
yinjianfei <1358009667@com>
2025-10-29 10:08:04 +0800

    seafile type add details dialog

--- frontend/src/project/main-panel/connections/components/row-details-dialog/index.js
+++ frontend/src/project/main-panel/connections/components/row-details-dialog/index.js
@@ -1,27 +1,27 @@
 import { Modal, ModalBody, UncontrolledTooltip } from 'reactstrap';
 import dayjs from 'dayjs';
 import { EmptyTip, ModalHeader, Icon } from '@/components';
-import { mediaUrl, gettext } from '@/constants';
+import { gettext, mediaUrl } from '@/constants';
 import { formatWithTimezone } from '@/sea-metadata/utils/column';
 
 import './index.css';
 
-const DiscourseForumsDetails = ({ rowDetailsTitle, rowDetails, onClose, handleSwitchRows }) => {
+const RowDetailsDialog = ({ rowDetailsTitle, rowDetails, onClose, handleSwitchRows }) => {
 
   return (
-    <Modal className='sea-qa-discourse-forums-details-container' isOpen={true} toggle={onClose} style={{ minWidth: 800 }}>
+    <Modal className='sea-qa-row-details-container' isOpen={true} toggle={onClose} style={{ minWidth: 800 }}>
       <ModalHeader toggle={onClose}>
         <div className="d-flex align-items-center">
           <div className="row-expand-direct-icons mr-2">
             <span
-              id="sea-qa-discourse-forums-details-prev-record-btn"
+              id="sea-qa-row-details-prev-record-btn"
               className="direct-icon rotate-icon-180"
               onClick={() => handleSwitchRows(-1)}
             >
               <Icon symbol="down" />
             </span>
             <span
-              id="sea-qa-discourse-forums-details-next-record-btn"
+              id="sea-qa-row-details-next-record-btn"
               className="direct-icon"
               onClick={() => handleSwitchRows(1)}
             >
@@ -29,7 +29,7 @@ const DiscourseForumsDetails = ({ rowDetailsTitle, rowDetails, onClose, handleSw
             </span>
             <UncontrolledTooltip
               placement="bottom"
-              target="sea-qa-discourse-forums-details-prev-record-btn"
+              target="sea-qa-row-details-prev-record-btn"
               fade={false}
               trigger="hover"
               className="sea-metadata-tooltip"
@@ -38,7 +38,7 @@ const DiscourseForumsDetails = ({ rowDetailsTitle, rowDetails, onClose, handleSw
             </UncontrolledTooltip>
             <UncontrolledTooltip
               placement="bottom"
-              target="sea-qa-discourse-forums-details-next-record-btn"
+              target="sea-qa-row-details-next-record-btn"
               fade={false}
               trigger="hover"
               className="sea-metadata-tooltip"
@@ -50,25 +50,22 @@ const DiscourseForumsDetails = ({ rowDetailsTitle, rowDetails, onClose, handleSw
         </div>
       </ModalHeader>
       <ModalBody>
-        <div className='sea-qa-discourse-forums-row-details'>
+        <div className='sea-qa-row-details'>
           {!rowDetails.length && <EmptyTip src={`${mediaUrl}img/no-items-tip.png`} />}
           {rowDetails.map(detail => (
-            <div key={detail.id} className='sea-qa-discourse-forums-reply-item'>
-              <div className='sea-qa-discourse-forums-reply-item-author-info-wrapper'>
-                <div className='sea-qa-discourse-forums-reply-item-author-info-left'>
-                  <div className='sea-qa-discourse-forums-reply-item-author-avatar'>
+            <div key={detail.id} className='sea-qa-row-details-reply-item'>
+              <div className='sea-qa-row-details-reply-item-author-info-wrapper'>
+                <div className='sea-qa-row-details-reply-item-author-info-left'>
+                  <div className='sea-qa-row-details-reply-item-author-avatar'>
                     <img alt='' src={`${mediaUrl}avatars/default.png`}/>
                   </div>
-                  <div className='sea-qa-discourse-forums-reply-item-author-name'>{detail.author}</div>
+                  <div className='sea-qa-row-details-reply-item-author-name'>{detail.author}</div>
                 </div>
-                <div
-                  className='sea-qa-discourse-forums-reply-item-author-time'
-                  title={formatWithTimezone(detail.updated_at)}
-                >
-                  {dayjs(detail.updated_at).format('YYYY-MM-DD HH:mm:ss')}
+                <div className='sea-qa-row-details-reply-item-author-time' title={formatWithTimezone(detail.time)}>
+                  {dayjs(detail.created_at).format('YYYY-MM-DD HH:mm:ss')}
                 </div>
               </div>
-              <div className='sea-qa-discourse-forums-reply-item-content' dangerouslySetInnerHTML={{ __html: detail.content }} />
+              <div className='sea-qa-row-details-reply-item-content' dangerouslySetInnerHTML={{ __html: detail.body }} />
             </div>
           ))}
         </div>
@@ -77,4 +74,4 @@ const DiscourseForumsDetails = ({ rowDetailsTitle, rowDetails, onClose, handleSw
   );
 };
 
-export default DiscourseForumsDetails;
+export default RowDetailsDialog;
