6fd8f060a958ade9356c97b19a12767b6a16b98e
yinjianfei <1358009667@com>
2025-11-06 17:36:27 +0800

    add hide and show button

--- frontend/src/project/main-panel/search/hidden-connection-popover/index.js
+++ frontend/src/project/main-panel/search/hidden-connection-popover/index.js
@@ -84,6 +84,14 @@ const HideConnectionPopover = ({ hidePopover, onChange, readOnly, target, placem
     update(newHiddenConnections);
   }, [hiddenConnectionIDs, update]);
 
+  const hideAll = useCallback(() => {
+    update(displayItems.map(connection => connection.id));
+  }, [hiddenConnectionIDs, update]);
+
+  const showAll = useCallback(() => {
+    update([]);
+  }, [update]);
+
   return (
     <UncontrolledPopover
       placement={placement}
@@ -110,6 +118,24 @@ const HideConnectionPopover = ({ hidePopover, onChange, readOnly, target, placem
           hiddenConnectionIDs={hiddenConnectionIDs}
           onChange={hideConnection}
         />
+        {!readOnly && !searchValue && (
+          <div className="seaqa-hide-connections-operations">
+            <div
+              className="sea-metadata-hide-connections-operation px-2"
+              onClick={hideAll}
+              aria-label={gettext('Hide all')}
+            >
+              {gettext('Hide all')}
+            </div>
+            <div
+              className="sea-metadata-hide-connections-operation px-2"
+              onClick={showAll}
+              aria-label={gettext('Show all')}
+            >
+              {gettext('Show all')}
+            </div>
+          </div>
+        )}
       </div>
     </UncontrolledPopover>
   );
