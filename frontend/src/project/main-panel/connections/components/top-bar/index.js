885d199d95baec6a0510dcd00920a905fee0f943
kaichen999 <113651927+kaichen999@users.noreply.github.com>
2025-11-17 16:27:56 +0800

    add-sync-now-button (#407)
    * sync-now-button
    
    * update
    
    * optimise codes
    
    ---------
    
    Co-authored-by: Michael An <1822852997@qq.com>

--- frontend/src/project/main-panel/connections/components/top-bar/index.js
+++ frontend/src/project/main-panel/connections/components/top-bar/index.js
@@ -1,17 +1,23 @@
 import React, { useCallback } from 'react';
 import { Button } from 'reactstrap';
+import dayjs from '@/utils/dayjs';
+import { Utils } from '@/utils/utils';
+import { connectionsAPI } from '../../../../api';
 import BasicTopBar from '../../../top-bar';
-import { useConnectionsPage } from '../../hooks';
+import { useConnectionsPage, useConnections } from '../../hooks';
 import { CONNECTION_PAGE_TYPE } from '../../constants';
-import { IconButton, Icon } from '@/components';
+import { IconButton, Icon, toaster } from '@/components';
 import { gettext } from '@/constants';
 import eventBus from '@/utils/event-bus';
 import { EVENT_BUS_TYPE } from '@/project/constants';
 
 import './index.css';
 
+const { projectUuid } = window.app.pageOptions;
+
 const TopBar = ({ title }) => {
   const { pageType, pageName, togglePageType } = useConnectionsPage();
+  const { modifyLocalConnectionRecord } = useConnections();
 
   const handleNewConnection = useCallback(() => {
     eventBus.dispatch(EVENT_BUS_TYPE.NEW_CONNECTION);
@@ -36,6 +42,23 @@ const TopBar = ({ title }) => {
     );
   }, [pageType, title, pageName, togglePageType]);
 
+  const onManualSync = useCallback((connectionID) => {
+    connectionsAPI.triggerSync(projectUuid, connectionID).then(() => {
+      toaster.success(gettext('Sync task queued'));
+      modifyLocalConnectionRecord(connectionID, { status: { last_sync_status: 'pending' } });
+    }).catch((error) => {
+      const errorMessage = Utils.getErrorMsg(error);
+      let error_msg = '';
+      if (errorMessage.message_type === 'Manual sync too frequent') {
+        const next_time = errorMessage.next_time ? dayjs(errorMessage.next_time).format('YYYY-MM-DD HH:mm:ss') : '--';
+        error_msg = errorMessage.message_type + '. Next time:' + next_time;
+      } else {
+        error_msg = errorMessage;
+      }
+      toaster.danger(error_msg);
+    });
+  }, [modifyLocalConnectionRecord]);
+
   const renderRightChildren = useCallback(() => {
     if (pageType === CONNECTION_PAGE_TYPE.ALL) {
       return (
@@ -44,8 +67,14 @@ const TopBar = ({ title }) => {
           {gettext('Add connection')}
         </Button>
       );
+    } else {
+      return (
+        <Button color="primary" className="sea-qa-project-add-connection-btn" onClick={() => { onManualSync(pageType);}}>
+          <Icon symbol="sync" className="mr-2" />
+          {gettext('Sync now')}
+        </Button>
+      );
     }
-    return null;
   }, [pageType, handleNewConnection]);
 
   return (
