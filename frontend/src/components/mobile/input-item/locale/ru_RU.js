0809d41485082fb3e6f6736ba7bd30e3487c5529
Michael An <1822852997@qq.com>
2025-11-19 11:51:26 +0800

    remove useless langs

--- frontend/src/components/mobile/input-item/locale/ru_RU.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  confirmLabel: 'Ок',
-  backspaceLabel: 'возврат на одну позицию',
-  cancelKeyboardLabel: 'Отменить клавиатуру',
-};
--- frontend/src/components/mobile/input-item/locale/sv_SE.js
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line import/no-anonymous-default-export
-export default {
-  confirmLabel: 'Ok',
-  backspaceLabel: 'Backspace',
-  cancelKeyboardLabel: 'CancelKeyboard',
-};
