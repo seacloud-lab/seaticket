6f9bb0c0d79c9a54e9ff57146825f40f32a93c87
zwfxxxxx <zwfxiaxue@163.com>
2025-11-22 17:46:07 +0800

    add AVAILABLE_LLM_MODELS from conf file

--- seahub/settings.py
+++ seahub/settings.py
@@ -657,10 +657,6 @@ AI_CHAT_GITHUB_ISSUE_MAX_COMMENTS_NUM = 20
 
 AI_PRICES = {}
 
-# Available AI Models for user selection
-AVAILABLE_LLM_MODELS = [
-    {'value': 'gpt-4o-mini', 'label': 'GPT-4o Mini'},
-]
 
 #####################
 # External settings #
@@ -708,6 +704,10 @@ else:
 # config in yaml & env
 yaml_file_path = os.path.join(CONF_DIR, os.environ.get('SEAQA_CONFIG_NAME', 'seaqa_config.yaml'))
 configs = ConfigParser(yaml_file_path, 'seaqa-web')
+ai_configs = ConfigParser(yaml_file_path, 'seaqa-ai')
+
+# Available AI Models for user selection
+AVAILABLE_LLM_MODELS = ai_configs.get('AVAILABLE_LLM_MODELS', [{'value': 'gpt-4o-mini', 'label': 'GPT-4o Mini'},])
 
 # jwt private key
 JWT_PRIVATE_KEY = configs.get('JWT_PRIVATE_KEY')
