f7cbce31307c790889cdec26dff92417d09e6548
‘JoinTyang’ <yangtong1009@163.com>
2025-11-24 10:47:49 +0800

    fix my ticket

--- seahub/tickets/ticket_utils.py
+++ seahub/tickets/ticket_utils.py
@@ -80,7 +80,7 @@ def get_tickets(seadb_api, project_uuid):
 def get_my_tickets(seadb_api, project_uuid, username, start, limit):
     from seahub.seadb_models.utils import get_tickets_columns
     sql = f"SELECT * FROM `{TABLE_TICKETS}` WHERE `deleted` = FALSE AND `state` = 'open' AND `participants` in ('{username}') LIMIT {limit} OFFSET {start}"
-    res = seadb_api.query_rows(project_uuid, sql)
+    res = seadb_api.query_rows(project_uuid, sql, convert_keys=False)
     records = res.get('results')
     columns = get_tickets_columns(seadb_api, project_uuid)
     display_columns = []
@@ -88,8 +88,6 @@ def get_my_tickets(seadb_api, project_uuid, username, start, limit):
         name = column['name']
         if name in TICKET_DISPLAY_ALL_COLUMNS:
             display_columns.append(column)
-    for record in records:
-        convert_ticket_select_column_name_to_option_id(columns, record)
     return records, display_columns
 
 def get_ticket_replies(seadb_api, project_uuid, ticket_id, start, end):
