ebe7e5895b1e21ae5ffad012b1a5fa48266f3574
Michael An <1822852997@qq.com>
2025-11-24 13:58:41 +0800

    Add Documents sub nav

--- frontend/src/project/side-panel/index.js
+++ frontend/src/project/side-panel/index.js
@@ -33,12 +33,15 @@ const SidePanel = ({ activeBar, toggleBar }) => {
             <Nav nav={BAR_TYPES[1]} activeBar={activeBar} level={1} onClick={toggleBar} />
             <ConnectionsNav nav={BAR_TYPES[2]} activeBar={activeBar} level={1} onClick={toggleBar} />
             <Nav nav={BAR_TYPES[3]} activeBar={activeBar} level={1} onClick={toggleBar} />
-            {isProjectAdmin && (
-              <Nav nav={BAR_TYPES[4]} activeBar={activeBar} level={1} onClick={toggleBar} />
-            )}
             <div className="sea-qa-project-side-panel-subtitle">{window.gettext('Tickets')}</div>
             <Nav nav={BAR_TYPES[5]} activeBar={activeBar} level={1} onClick={toggleBar} />
             <Nav nav={BAR_TYPES[6]} activeBar={activeBar} level={1} onClick={toggleBar} />
+            {isProjectAdmin &&
+              <>
+                <div className="sea-qa-project-side-panel-subtitle">{window.gettext('Documents')}</div>
+                <Nav nav={BAR_TYPES[4]} activeBar={activeBar} level={1} onClick={toggleBar} />
+              </>
+            }
           </div>
         </div>
         <ResizeBar min={200} max={600} onResize={onResize} />
