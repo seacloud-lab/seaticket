373b39e2dcfeec997f15f08755394f7d0a090434
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2025-11-27 16:32:20 +0800

    feat: optimize code

--- frontend/src/project/main-panel/connections/components/cell-formatter/date-formatter.js
+++ frontend/src/project/main-panel/connections/components/cell-formatter/date-formatter.js
@@ -1,7 +1,7 @@
 import { formatWithTimezone, getDateDisplayString } from '@/sea-metadata/utils/column';
 
 const DateFormatter = ({ value, column }) => {
-  if (!value) return '---';
+  if (!value) return '--';
   return (
     <span title={formatWithTimezone(value)}>
       {getDateDisplayString(value, column?.data?.format || 'YYYY-MM-DD HH:mm:ss')}
