84dc933ee46dc2cc045b3935f256083addd6c69a
sunjinlong <kaichen999@outlook.com>
2025-12-01 17:54:39 +0800

    Update utils.py

--- seahub/project/utils.py
+++ seahub/project/utils.py
@@ -482,12 +482,12 @@ def ticket_to_json(project_uuid, ticket_id):
     for ticket_comment in ticket_comments:
         nickname = nickname_map.get(ticket_comment.get('creator'))
         content = ticket_comment.get('content')
-        replied_at = ticket_comment.get('created_time')
-        replied_at = time_str_to_utc_time(replied_at).isoformat()
+        commented_at = ticket_comment.get('created_time')
+        commented_at = time_str_to_utc_time(commented_at).isoformat()
         whole_ticket_data['comments'].append({
             'nickname': nickname,
             'content': content,
-            'replied_at': replied_at
+            'commented_at': commented_at
         })
     return json.dumps(whole_ticket_data, indent=4)
 
