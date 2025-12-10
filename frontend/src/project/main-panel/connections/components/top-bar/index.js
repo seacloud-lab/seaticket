d2a1eea53b50c08c08066c948d0b2fcd7d235402
yinjianfei-user <85612278+yinjianfei-user@users.noreply.github.com>
2025-12-10 17:12:37 +0800

    Optimize sync now (#570)
    * optimize sync now
    
    * optimize sync now
    
    ---------
    
    Co-authored-by: yinjianfei <1358009667@com>

--- frontend/src/project/main-panel/connections/components/top-bar/index.js
+++ frontend/src/project/main-panel/connections/components/top-bar/index.js
@@ -1,11 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import dayjs from '@/utils/dayjs';
+import { Button } from 'reactstrap';
 import { Utils } from '@/utils/utils';
 import { connectionsAPI } from '../../../../api';
 import BasicTopBar from '../../../top-bar';
 import { useConnectionsPage, useConnections } from '../../hooks';
 import { CONNECTION_PAGE_SLUG_ID, CONNECTION_TYPE } from '../../constants';
-import { IconButton, toaster } from '@/components';
+import { IconButton, toaster, CenteredLoading } from '@/components';
 import { gettext } from '@/constants';
 import eventBus from '@/utils/event-bus';
 import { EVENT_BUS_TYPE } from '@/project/constants';
@@ -17,9 +18,11 @@ import './index.css';
 const { projectUuid } = window.app.pageOptions;
 
 const TopBar = ({ title, modifyLocalBar }) => {
-  const { pageSlugId, connectionInfo, togglePageSlugId } = useConnectionsPage();
+  const { pageSlugId, connectionInfo, togglePageSlugId, onRefresh } = useConnectionsPage();
   const { modifyLocalConnectionRecord } = useConnections();
   const { name: connectionName, type: connectionType } = connectionInfo || {};
+  const timer = useRef(null);
+  const [isSyncing, setIsSyncinig] = useState(false);
 
   const handleNewConnection = useCallback(() => {
     eventBus.dispatch(EVENT_BUS_TYPE.NEW_CONNECTION);
@@ -53,9 +56,35 @@ const TopBar = ({ title, modifyLocalBar }) => {
     );
   }, [pageSlugId, title, connectionName, handleReturnConnectionsHome]);
 
+  const onQueryConnectionStatus = useCallback((connectionID) => {
+    timer.current = setTimeout(() => {
+      connectionsAPI.getConnection(projectUuid, connectionID).then((res) => {
+        const { last_sync_status, last_sync_count } = JSON.parse(res.data.record.status);
+        if (last_sync_status === 'completed') {
+          if (last_sync_count > 0) {
+            onRefresh();
+            const msg = gettext('%s records synced').replace('%s', last_sync_count);
+            toaster.success(msg);
+          } else {
+            toaster.success(gettext('No new records'));
+          }
+          clearTimeout(timer.current);
+          setIsSyncinig(false);
+        } else {
+          onQueryConnectionStatus(connectionID);
+        }
+      }).catch((error) => {
+        setIsSyncinig(false);
+        const errorMessage = Utils.getErrorMsg(error);
+        toaster.danger(errorMessage);
+      });
+    }, 3000);
+  }, [projectUuid]);
+
   const onManualSync = useCallback((connectionID) => {
     connectionsAPI.triggerSync(projectUuid, connectionID).then(() => {
-      toaster.success(gettext('Sync task queued'));
+      setIsSyncinig(true);
+      onQueryConnectionStatus(connectionID);
       modifyLocalConnectionRecord(connectionID, { status: { last_sync_status: 'pending' } });
     }).catch((error) => {
       const errorMessage = Utils.getErrorMsg(error);
@@ -66,9 +95,16 @@ const TopBar = ({ title, modifyLocalBar }) => {
       } else {
         error_msg = errorMessage;
       }
+      setIsSyncinig(false);
       toaster.danger(error_msg);
     });
   }, [modifyLocalConnectionRecord]);
+
+  useEffect(() => {
+    return () => {
+      timer.current && clearTimeout(timer.current);
+    };
+  }, []);
 
   const renderRightChildren = useCallback(() => {
     if (pageSlugId === CONNECTION_PAGE_SLUG_ID.ALL) {
@@ -79,10 +115,18 @@ const TopBar = ({ title, modifyLocalBar }) => {
         {(connectionType === CONNECTION_TYPE.GITHUB_ISSUE || connectionType === CONNECTION_TYPE.DISCOURSE_FORUM) && (
           <AddButton onClick={handleOpenConnectionEmbeddingVisualizationOpen} text={gettext('Analyze')} className="mr-4" />
         )}
-        <AddButton onClick={() => onManualSync(pageSlugId)} text={gettext('Sync now')} icon="sync" />
+        {isSyncing
+          ?
+          (
+            <Button className='sea-qa-project-syncing-connections-btn' disabled={true} color="primary">
+              <CenteredLoading className='mr-2' />
+              <span>{gettext('Syncing')}</span>
+            </Button>
+          )
+          : <AddButton onClick={() => onManualSync(pageSlugId)} text={gettext('Sync now')} icon="sync" />}
       </>
     );
-  }, [pageSlugId, connectionType, handleNewConnection, onManualSync, handleOpenConnectionEmbeddingVisualizationOpen]);
+  }, [pageSlugId, connectionType, isSyncing, handleNewConnection, onManualSync, handleOpenConnectionEmbeddingVisualizationOpen]);
 
   return (
     <BasicTopBar>
