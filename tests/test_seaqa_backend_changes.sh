dfed324ccc3e30abe7eccc67c5862ea4ab946207
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2026-01-04 18:20:48 +0800

    update package

---
+++ tests/test_seaqa_backend_changes.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+set -e
+
+test_fn()
+{
+    FN=$1
+    shift;
+    echo "Testing $FN..."
+
+    patt="frontend*"
+    [[ $FN == $patt ]] && return 0
+
+    patt="media*"
+    [[ $FN == $patt ]] && return 0
+
+    patt="static*"
+    [[ $FN == $patt ]] && return 0
+
+    patt="locale*"
+    [[ $FN == $patt ]] && return 0
+
+    return 1
+}
+
+if test -z "$GITHUB_BASE_REF"; then
+    echo "GITHUB_BASE_REF is empty, run backend tests by default."
+    exit 0
+fi
+
+git fetch origin $GITHUB_BASE_REF
+FILES=`git diff --name-only origin/$GITHUB_BASE_REF`
+
+echo "$FILES"
+
+for i in $FILES
+do
+    test_fn $i
+    retval=$?
+
+    if [ "$retval" == 1 ]; then
+        echo "File changes need to trigger backend tests."
+        exit 0
+    fi
+
+done
+
+echo "Static/media/frontend/locale only changes should not trigger backend tests."
+exit 1
