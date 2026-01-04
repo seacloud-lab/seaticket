2e9ca454b7da0ca319d7e4cc72d020cb98b23358
孙永强 <11704063+s-yongqiang@user.noreply.gitee.com>
2026-01-04 12:03:26 +0800

    add internal api

--- seahub/api2/urls.py
+++ seahub/api2/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     re_path(r'^ping/$', Ping.as_view()),
     re_path(r'^auth/ping/$', AuthPing.as_view()),
     re_path(r'^auth-token/', ObtainAuthToken.as_view()),
+    re_path(r'^internal/send-email-notices/$', SendEmailNoticesInternal.as_view()),
     re_path(r'^two-factor-auth/(?P<email>\S+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/$', TwoFactorAuthView.as_view(), name="two-factor-auth-view"),
 
     # RESTful API
