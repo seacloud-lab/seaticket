71f24ce39e9ca6bfb569be6b3ad39eaaba84747f
yinjianfei <1358009667@com>
2026-01-10 15:28:29 +0800

    remove empty module

--- frontend/src/project/main-panel/inbox/components/inbox-notification-item.js
+++ frontend/src/project/main-panel/inbox/components/inbox-notification-item.js
@@ -1,6 +1,5 @@
 import React, { useMemo, useCallback } from 'react';
 import PropTypes from 'prop-types';
-import { Trans } from 'react-i18next';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 import { gettext, siteRoot, mediaUrl } from '@constants/config';
