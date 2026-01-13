8a1b1976af0a9e91aafe01c5279d9f4e60a00772
cir9no <cir9no@gmail.com>
2026-01-13 14:16:35 +0800

    optimize code

--- sql/mysql.sql
+++ sql/mysql.sql
@@ -669,4 +669,4 @@ CREATE TABLE `invitations_invitation` (
   PRIMARY KEY (`id`),
   KEY `invitations_invitation_inviter_b0a7b855` (`inviter`),
   KEY `invitations_invitation_token_25a92a38` (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
