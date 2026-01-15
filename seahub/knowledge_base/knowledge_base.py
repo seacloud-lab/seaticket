dce95666320a34b0fe61090cc987679301e26693
JunhuiZhang <junhuizhang42@gmail.com>
2026-01-15 13:44:20 +0800

    move tag arg check

--- seahub/knowledge_base/knowledge_base.py
+++ seahub/knowledge_base/knowledge_base.py
@@ -271,6 +271,13 @@ class KnowledgeBaseAPIView(APIView):
         row = {}
         username = request.user.username
 
+        if 'tags' in request.data:
+            tags = request.data.get('tags')
+            if not isinstance(tags, list):
+                error_msg = 'tags invalid.'
+                return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
+            row[KnowledgeBaseTable.tags.name] = tags        
+
         if 'title' in request.data:
             title = request.data.get('title')
             if not title:
@@ -301,13 +308,6 @@ class KnowledgeBaseAPIView(APIView):
                     error_msg = 'Upload files failed.'
                     return api_error(status.HTTP_500_INTERNAL_SERVER_ERROR, error_msg)
             row[KnowledgeBaseTable.content.name] = content_text
-
-        if 'tags' in request.data:
-            tags = request.data.get('tags')
-            if not isinstance(tags, list):
-                error_msg = 'tags invalid.'
-                return api_error(status.HTTP_400_BAD_REQUEST, error_msg)
-            row[KnowledgeBaseTable.tags.name] = tags
 
         project = Projects.objects.get_project_by_uuid(project_uuid)
         if not project:
