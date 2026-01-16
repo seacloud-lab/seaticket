fce38d2e50a446ed6d42c85301b240b4d6a8db03
Michael An <1822852997@qq.com>
2026-01-16 11:28:48 +0800

    change account popover style

--- frontend/src/components/account/index.js
+++ frontend/src/components/account/index.js
@@ -208,7 +208,7 @@ class Account extends Component {
               <img src={avatarURL} width="32" height="32" className="avatar" alt={gettext('Avatar')} />
               <div className="txt text-truncate mb-4">{this.state.userName}</div>
             </div>
-            <div className="item pt-3 pb-1">
+            <div className="item pt-3 pb-0">
               <div className="account-info-card">
                 <p className='account-info-card-title'>{gettext('API calls count')}</p>
                 <p>
