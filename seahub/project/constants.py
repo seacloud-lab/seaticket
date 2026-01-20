6161b8ebe9f12f2ef3b1df31a7edefcc2c0a8e15
Jinlong Sun <kaichen999@outlook.com>
2026-01-20 17:22:37 +0800

    optimize kb records display

--- seahub/project/constants.py
+++ seahub/project/constants.py
@@ -311,7 +311,7 @@ CONNECTION_MUST_RETURN_COLUMNS = {
 LLM_INPUT_CHARACTERS_LIMIT = 4000
 
 
-KNOWLEDGE_BASE_DISPLAY_ALL_COLUMNS = ['_pk', 'title', 'content', 'tags', 'ai_summary', 'ai_processed_time', 'creator', 'created_time', 'last_modifier', 'modified_time']
+KNOWLEDGE_BASE_DISPLAY_ALL_COLUMNS = ['_pk', 'title', 'tags', 'ai_summary', 'ai_processed_time', 'creator', 'created_time', 'last_modifier', 'modified_time']
 
 
 # Connection categories
