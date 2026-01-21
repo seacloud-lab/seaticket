223d4148b190f208590a01f7b3d3ad4987acbba2
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2026-01-21 14:18:30 +0800

    feat: resource internalNetworkAddress

--- frontend/src/project/utils.js
+++ frontend/src/project/utils.js
@@ -3,7 +3,6 @@ import { KNOWLEDGE_BASE_TYPE } from './main-panel/knowledge-base/constants';
 import { generatorKnowledgeBaseURL } from './main-panel/knowledge-base/utils';
 import { TICKET_TYPE } from './main-panel/tickets/constants';
 import { getConnectionIcon, getOriginalPageUrl } from './main-panel/connections/utils';
-import { CONNECTION_TYPE } from './main-panel/connections/constants';
 import { generatorTicketURL } from './main-panel/tickets/utils';
 
 export const getResourceIconURL = (type) => {
@@ -13,18 +12,14 @@ export const getResourceIconURL = (type) => {
   return getConnectionIcon(type);
 };
 
-export const getResourceURL = (type, resourceID, { url, workspaceID, projectName, connectionID }) => {
-  if (url) return url;
+export const getInternalNetworkAddress = (type, resourceID, { workspaceID, projectName, connectionID }) => {
   if (type === TICKET_TYPE) return generatorTicketURL({ ticket: { _id: resourceID, }, workspaceID, projectName });
   if (type === KNOWLEDGE_BASE_TYPE) return generatorKnowledgeBaseURL({ kb: { _id: resourceID }, workspaceID, projectName });
   const baseURL = location.origin + '/workspace/' + workspaceID + '/project/' + projectName + '/';
-  if (type === CONNECTION_TYPE.EMAIL) return `${baseURL}connections/${connectionID}/records/${resourceID}/`;
-  return `${baseURL}connections/${connectionID}/`;
+  return `${baseURL}connections/${connectionID}/records/${resourceID}/`;
 };
 
 export const getResourceOriginalURL = (type, resource, { workspaceID, projectName, connections, columns }) => {
-  // if (type === TICKET_TYPE) return generatorTicketURL({ ticket: resource, workspaceID, projectName });
-  // if (type === KNOWLEDGE_BASE_TYPE) return generatorKnowledgeBaseURL({ kb: resource, workspaceID, projectName });
   if (type === TICKET_TYPE || type === KNOWLEDGE_BASE_TYPE) return '';
   const connection = connections.find(c => c.id === resource.connection_id);
   if (!connection) return '';
