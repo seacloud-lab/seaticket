4d9c91a7ac83a93cbeae5bd734d0825d0171534f
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2026-02-02 10:31:53 +0800

    fix: portal provider

--- frontend/src/portal/index.js
+++ frontend/src/portal/index.js
@@ -2,16 +2,12 @@ import React, { useState, useCallback, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import { I18nextProvider } from 'react-i18next';
 import i18n from '../_i18n/i18n-seafile-editor';
-import { CollaboratorsProvider } from '@/sea-metadata';
-import userAPI from '@/api/user-api';
-import projectAPI from '@/project/api/project-api';
 import LeftBar from './left-bar';
 import SidePanel from './side-panel';
 import MainPanel from './main-panel';
 import { CenteredLoading } from '../components';
 import { PORTAL_PAGE } from './constants';
-import { MetadataProvider } from '@/project/main-panel/tickets/hooks';
-import { TagsProvider } from '@/project/main-panel/tags/hooks';
+import { DataProvider } from '@/project/hooks';
 
 import './index.css';
 
@@ -29,14 +25,6 @@ const Portal = () => {
     const url = `${origin}/${basePath}/${projectUuid}/${page}/`;
     history.replaceState(null, null, url);
   }, []);
-
-  const listUserInfo = useCallback((...params) => {
-    return userAPI.listUserInfo(...params);
-  }, []);
-
-  const getCollaborators = useCallback(() => {
-    return projectAPI.listProjectRelatedUsers(projectUuid);
-  }, [projectUuid]);
 
   useEffect(() => {
     const { pathname } = location;
@@ -59,23 +47,19 @@ const Portal = () => {
 
   return (
     <I18nextProvider i18n={i18n}>
-      <CollaboratorsProvider listUserInfo={listUserInfo} getCollaborators={getCollaborators}>
-        <TagsProvider projectUuid={projectUuid}>
-          <MetadataProvider projectUuid={projectUuid}>
-            <div className="sea-qa-portal">
-              {isLoading ? (
-                <CenteredLoading />
-              ) : (
-                <>
-                  {isEditMode && <LeftBar />}
-                  <SidePanel activePage={activePage} onPageChange={onPageChange} enableKB={enableKB} />
-                  <MainPanel activePage={activePage} projectUuid={projectUuid} onPageChange={onPageChange} />
-                </>
-              )}
-            </div>
-          </MetadataProvider>
-        </TagsProvider>
-      </CollaboratorsProvider>
+      <DataProvider projectUuid={projectUuid}>
+        <div className="sea-qa-portal">
+          {isLoading ? (
+            <CenteredLoading />
+          ) : (
+            <>
+              {isEditMode && <LeftBar />}
+              <SidePanel activePage={activePage} onPageChange={onPageChange} enableKB={enableKB} />
+              <MainPanel activePage={activePage} projectUuid={projectUuid} onPageChange={onPageChange} />
+            </>
+          )}
+        </div>
+      </DataProvider>
     </I18nextProvider>
   );
 };
