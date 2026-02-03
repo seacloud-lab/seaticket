85d56aae7d02c80e52570e1ce8541d2953ff6e4a
yinjianfei <1358009667@com>
2026-02-03 18:02:52 +0800

    hidden tag description

--- frontend/src/components/tag-option/index.jsx
+++ frontend/src/components/tag-option/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import './index.css';
 
-const TagOption = ({ tag = {} }) => {
+const TagOption = ({ tag = {}, hiddenDescription = false }) => {
   const { color, name, description } = tag;
   return (
     <>
       <div className="sea-qa-tags-selector-tag-bg" style={{ backgroundColor: color }}></div>
       <div className="sea-qa-tags-selector-tag-name-description">
         <div className="sea-qa-tags-selector-tag-name">{name}</div>
-        {description && (<div className="sea-qa-tags-selector-tag-description">{description}</div>)}
+        {!hiddenDescription && description && (<div className="sea-qa-tags-selector-tag-description">{description}</div>)}
       </div>
     </>
   );
