e6f37aca15478a5cd208e8f64336a1bd98f23d1c
Michael An <2331806369@qq.com>
2026-02-04 12:01:46 +0800

    Change calendar style (#885)
    * change calendar customize style
    
    * update calendar version

--- frontend/src/sea-metadata/components/popover/filter-popover/advanced-filters/filter-calendar.js
+++ frontend/src/sea-metadata/components/popover/filter-popover/advanced-filters/filter-calendar.js
@@ -118,6 +118,7 @@ class FilterCalendar extends Component {
         <DatePicker
           calendar={
             <Calendar
+              className="sea-ticket-calendar"
               locale={translateCalendar(this.lang)}
               style={{ zIndex: zIndex || 1001 }}
               dateInputPlaceholder={gettext('Enter date')}
