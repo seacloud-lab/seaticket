14a2dbaff2dfcd045f731355e0a9632ae04d0783
Michael An <1822852997@qq.com>
2026-02-05 10:43:58 +0800

    change option sequence

--- frontend/src/project/main-panel/analyze/index.js
+++ frontend/src/project/main-panel/analyze/index.js
@@ -85,6 +85,7 @@ const Analyze = ({ title }) => {
       return {};
     }
     const options = {};
+    options['state'].push('all');
     FILTERABLE_FIELDS.forEach(({ field }) => {
       const values = records.map(r => r[field]);
       const uniqueValues = [...new Set(values.filter(v => v != null))].sort();
@@ -92,7 +93,6 @@ const Analyze = ({ title }) => {
         options[field] = uniqueValues;
       }
     });
-    options['state'].push('all');
     return options;
   }, [records, FILTERABLE_FIELDS]);
 
