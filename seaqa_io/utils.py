1c4ffa3598bb49b27b191b36f46459e66b7d7b90
JunhuiZhang <junhuizhang42@gmail.com>
2026-02-10 13:57:24 +0800

    add get_connection_table_name() back for seaqa_io

--- seaqa_io/utils.py
+++ seaqa_io/utils.py
@@ -16,3 +16,17 @@ def uuid_str_to_32_chars(project_uuid):
     if len(project_uuid) == 36:
         return uuid.UUID(project_uuid).hex
     return project_uuid
+
+def get_connection_table_name(connection_type, connection_id):
+    table_name = ''
+    if connection_type == ConnectionType.GITHUB_ISSUE.value:
+        table_name = ConnectionType.GITHUB_ISSUE.value + '_' + str(connection_id)
+    elif connection_type == ConnectionType.DISCOURSE_FORUM.value:
+        table_name = ConnectionType.DISCOURSE_FORUM.value + '_' + str(connection_id)
+    elif connection_type == ConnectionType.SEAFILE.value:
+        table_name = ConnectionType.SEAFILE.value + '_' + str(connection_id)
+    elif connection_type == ConnectionType.SITE.value:
+        table_name = ConnectionType.SITE.value + '_' + str(connection_id)
+    elif connection_type == ConnectionType.EMAIL.value:
+        table_name = ConnectionType.EMAIL.value + '_' + 'thread_' + str(connection_id)
+    return table_name
