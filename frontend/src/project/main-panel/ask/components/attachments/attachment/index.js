ec6a3b88d48cb99cd9f863ada92172b2b5e80ead
杨国璇 <37972689+YangGuoXuan-0503@users.noreply.github.com>
2026-02-26 18:00:54 +0800

    feat: attachemnt remove btn UI

--- frontend/src/project/main-panel/ask/components/attachments/attachment/index.js
+++ frontend/src/project/main-panel/ask/components/attachments/attachment/index.js
@@ -1,5 +1,6 @@
 import React, { useCallback, useMemo } from 'react';
-import { ClearIconButton, Icon } from '@/components';
+import { IconTooltip, Icon } from '@/components';
+import classnames from 'classnames';
 import { gettext } from '@/constants';
 import { AttachmentObject } from '../../../models';
 
@@ -24,15 +25,18 @@ const Attachment = ({ value, index, onRemove, openAttachment }) => {
   const { icon, title } = attachment;
 
   return (
-    <div className="sea-qa-ai-chat-attachment" onClick={onClick}>
+    <div className={classnames('sea-qa-ai-chat-attachment', { 'sea-qa-ai-chat-attachment-remove-able': onRemove })} onClick={onClick}>
       <Icon symbol={icon} className={`sea-qa-project-ticket-state-${icon}-icon sea-qa-project-ai-attachment-icon mr-2`} />
       <span className="text-truncate flex-1" title={title} aria-label={title}>{title}</span>
       {onRemove && (
-        <ClearIconButton
-          useTooltip={true}
-          className="mr-0 ml-4"
-          title={gettext('Remove')}
+        <IconTooltip
+          hoverBackground={true}
+          icon="close"
+          className="mr-0 sea-qa-ai-chat-attachment-remove-btn"
+          tip={gettext('Remove')}
+          size={{ btn: 24, icon: 12 }}
           onClick={handleRemove}
+          placement="top"
         />
       )}
     </div>
