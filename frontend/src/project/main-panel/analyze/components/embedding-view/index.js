20cb197a7f23e1fe6ddd67e22bf91c09dc823307
yinjianfei-user <85612278+yinjianfei-user@users.noreply.github.com>
2026-03-02 17:12:07 +0800

    update empty state (#974)
    * update empty state
    
    * remove empty code
    
    * change tip text
    
    ---------
    
    Co-authored-by: yinjianfei <1358009667@com>
    Co-authored-by: Michael An <2331806369@qq.com>

--- frontend/src/project/main-panel/analyze/components/embedding-view/index.js
+++ frontend/src/project/main-panel/analyze/components/embedding-view/index.js
@@ -5,7 +5,7 @@ import * as SQL from '@uwdata/mosaic-sql';
 import ResourceDetailsDialog from '@/project/components/resource-details-dialog';
 import Legend from '../legend';
 import { CenteredError, CenteredLoading, EmptyTip } from '@/components';
-import { gettext } from '@/constants';
+import { gettext, mediaUrl } from '@/constants';
 import { TABLE_SCHEMA, COLOR_BY_FIELDS } from '../../constants';
 
 import './index.css';
@@ -338,9 +338,11 @@ const EmbeddingView = ({
 
   if (!Array.isArray(records)) {
     return (
-      <div className="analyze-empty-state">
-        <p className="analyze-empty-text">{gettext('Click Analyze to start')}</p>
-      </div>
+      <EmptyTip
+        src={`${mediaUrl}img/no-analysis.png`}
+        title={gettext('No analysis')}
+        text={gettext('Click analyze to start')}
+      />
     );
   }
 
