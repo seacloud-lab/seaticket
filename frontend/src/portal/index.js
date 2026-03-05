6e6d94f7aa928d3674b2239d81c1a3678836d3e3
zwfxxxxx <zwfxiaxue@163.com>
2026-03-05 13:42:26 +0800

    external members support chat

--- frontend/src/portal/index.js
+++ frontend/src/portal/index.js
@@ -40,7 +40,6 @@ const Portal = () => {
       location.href = `${origin}/portal/${projectUuid}/login/`;
       return;
     }
-    if (isExternalUser && page === PORTAL_PAGE.CHAT) return;
     if (!enableKB && page === PORTAL_PAGE.KNOWLEDGE_BASE) return;
     setActivePage(page);
     const { origin } = location;
@@ -59,8 +58,6 @@ const Portal = () => {
       if (Object.values(PORTAL_PAGE).includes(pageKey)) {
         const isRestrictedPage = pageKey === PORTAL_PAGE.SUBMIT_TICKET || pageKey === PORTAL_PAGE.MY_TICKETS || pageKey === PORTAL_PAGE.CHAT;
         if (isAnonymous && isRestrictedPage) {
-          setActivePage(getDefaultPage(showKBInPortal, isAnonymous));
-        } else if (isExternalUser && pageKey === PORTAL_PAGE.CHAT) {
           setActivePage(getDefaultPage(showKBInPortal, isAnonymous));
         } else if (!enableKB && pageKey === PORTAL_PAGE.KNOWLEDGE_BASE) {
           setActivePage(getDefaultPage(showKBInPortal, isAnonymous));
