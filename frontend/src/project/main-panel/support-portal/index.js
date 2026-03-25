23140e6365eae84b0fb893b4100afbe56a6b8e64
Michael An <2331806369@qq.com>
2026-03-25 16:28:00 +0800

    fix support portal page permission (#1112)

--- frontend/src/project/main-panel/support-portal/index.js
+++ frontend/src/project/main-panel/support-portal/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import TopBar from './top-bar';
 import View from './view';
-
-const { projectUuid, permission } = window.app.pageOptions;
+import TopBar from '../top-bar';
 
 const SupportPortal = ({ title }) => {
   return (
     <>
-      <TopBar title={title} />
-      <View projectUuid={projectUuid} permission={permission} />
+      <TopBar>
+        <span className="text-truncate" title={title}>{title}</span>
+      </TopBar>
+      <View />
     </>
   );
 };
