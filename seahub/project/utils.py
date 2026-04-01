04aa5c20ddd71dcf410033be64cc159a1db4a9f8
awu0403 <76416779+awu0403@users.noreply.github.com>
2026-04-01 13:55:01 +0800

    optimize delete project

--- seahub/project/utils.py
+++ seahub/project/utils.py
@@ -6,7 +6,7 @@ from urllib.parse import quote_plus
 from seahub.project.models import Projects, DeletedProjects, ConnectionsViews, \
     AIUsageStatistics, AIUsageStatistics, Workspaces, ProjectIssuesStatistics
 from seahub.chats.models import ChatSessions, ChatMessages, ChatMessageThoughtProcess
-from seahub.portal.models import PortalChatSessions, PortalChatMessages
+from seahub.portal.models import PortalChatSessions, PortalChatMessages, PortalExternalInvitation
 from seahub.tickets.models import TicketViews
 from seahub.knowledge_base.models import KnowledgeBaseViews
 from django.db.models import Sum, Value
@@ -205,18 +205,17 @@ def delete_project(project):
         TicketViews.objects.filter(project_uuid=project_uuid).delete()
         KnowledgeBaseViews.objects.filter(project_uuid=project_uuid).delete()
         ProjectNotification.objects.filter(project_uuid=project_uuid).delete()
+        PortalExternalInvitation.objects.filter(project_uuid=project_uuid).delete()
+        ProjectIssuesStatistics.objects.filter(project_uuid=project_uuid).delete()
         session_uuids = ChatSessions.objects.filter(project_uuid=project_uuid).values_list('session_uuid', flat=True)
         delete_sessions(session_uuids)
         portal_session_uuids = PortalChatSessions.objects.filter(project_uuid=project_uuid).values_list('session_uuid', flat=True)
         delete_portal_sessions(portal_session_uuids)
-        seadb_api = SeaDBAPI()
-        seadb_api.delete_base(project_uuid)
     except Exception as e:
         logger.error(e)
 
     try:
         Projects.objects.delete_project(project.workspace, project.name)
-        ProjectIssuesStatistics.objects.filter(project_uuid=project_uuid).delete()
         DeletedProjects(project_uuid=project_uuid).save()
     except Exception as e:
         logger.error('delete project: %s error: %s', str(project_uuid), e)
