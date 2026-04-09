688ed4e9248d4b35e4b85fea337d17e53e2b955b
yinjianfei <1358009667@qq.com>
2026-04-09 13:49:06 +0800

    code optimize

--- frontend/src/components/chart/constants/index.js
+++ frontend/src/components/chart/constants/index.js
@@ -3,30 +3,6 @@ export const STYLE_COLORS = [
     name: 'theme0',
     colors: ['#5B8FF9', '#5AD8A6', '#5D7092', '#F6BD16', '#6F5EF9', '#6DC8EC', '#945FB9', '#FF9845', '#1E9493', '#FF99C3'],
   },
-  {
-    name: 'theme1',
-    colors: ['#FF6A3A', '#626681', '#FFC002', '#9FB310', '#8D674D', '#DAD5B5', '#0D8E89', '#F79539', '#FA7298', '#247FEA'],
-  },
-  {
-    name: 'theme2',
-    colors: ['#FFC61B', '#FE693B', '#B8CEE0', '#FDC585', '#FFA03F', '#9095A0', '#FFAB90', '#E4785D', '#FFC46F', '#FFDCA8'],
-  },
-  {
-    name: 'theme3',
-    colors: ['#388DFF', '#BE74FD', '#FF86AD', '#FFE05D', '#9EFDC7', '#FFBCCC', '#BDAEFF', '#74DAF3', '#FFE5A5', '#97BCFF'],
-  },
-  {
-    name: 'theme4',
-    colors: ['#6774C3', '#FF95D2', '#FACF5B', '#3FBFE4', '#6D8492', '#BDAEFF', '#FFBCCC', '#ADDF84', '#FFC88C', '#97BCFF'],
-  },
-  {
-    name: 'theme5',
-    colors: ['#5562E1', '#49D096', '#FF9750', '#FFBF5C', '#8490EC', '#FCA3D6', '#FFD637', '#5AAEF3', '#5B6E96', '#97B1FF'],
-  },
-  {
-    name: 'theme6',
-    colors: ['#F76856', '#FFAF53', '#6CCDFF', '#C4C4CC', '#D48C82', '#E3C19A', '#A47CFF', '#FFD637', '#96DCFF', '#FFB6B6'],
-  }
 ];
 
 export const CHART_THEME_COLOR = {
