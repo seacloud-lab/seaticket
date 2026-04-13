68aeb1cef9aa332eed7c11aed01ae20393aab83e
lian <imwhatiam123@gmail.com>
2026-04-13 17:18:20 +0800

    SEAFILE_CENTRAL_CONF_DIR -> CONF_PATH

--- seahub/organizations/__init__.py
+++ seahub/organizations/__init__.py
@@ -3,7 +3,7 @@ import sys
 from seahub.organizations.signals import org_operation_signal
 
 try:
-    conf_dir = os.environ['SEAFILE_CENTRAL_CONF_DIR']
+    conf_dir = os.environ['CONF_PATH']
     sys.path.append(conf_dir)
     from seahub_custom_functions import org_operation_callback
     org_operation_signal.connect(org_operation_callback)
