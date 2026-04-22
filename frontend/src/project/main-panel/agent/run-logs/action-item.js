7edaee2a409693c9cfc1b4133893f6a734ea2402
Michael An <2331806369@qq.com>
2026-04-22 10:18:12 +0800

    change Agent log Analysis UI (#1264)

--- frontend/src/project/main-panel/agent/run-logs/action-item.js
+++ frontend/src/project/main-panel/agent/run-logs/action-item.js
@@ -121,7 +121,11 @@ const ActionItem = React.memo(({
         return (
           <div className="action-content action-content-analysis">
             <div className="action-label">{gettext('Analysis')}</div>
-            <div className="action-text">{content}</div>
+            <div className="action-text">
+              {content.split('\n').map((item, index) => (
+                <p key={index}>{item}</p>
+              ))}
+            </div>
           </div>
         );
       case ACTION_TYPE.TOOL_CALL:
