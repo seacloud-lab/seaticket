3697339a0fb8f6bdcd8153a202d3df5a5c544e8e
Michael An <1822852997@qq.com>
2026-05-06 16:57:17 +0800

    change support portal page login

--- frontend/src/portal/side-panel/index.js
+++ frontend/src/portal/side-panel/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Button } from 'reactstrap';
 import { gettext, siteRoot } from '@/constants';
-import { CustomizeTabs, Icon } from '@/components';
+import { CustomizeTabs } from '@/components';
 import { PORTAL_PAGE, TICKETS_TAB, BASE_PRIMARY_TABS } from '../constants';
+import Account from '@/components/account';
 
 const SidePanel = ({ activePage, onPageChange, enableKB, isAnonymous }) => {
   const primaryTabs = isAnonymous
@@ -23,7 +25,7 @@ const SidePanel = ({ activePage, onPageChange, enableKB, isAnonymous }) => {
     onPageChange(value);
   };
 
-  const { isEditMode, projectUuid, isExternalUser, username } = window.app.pageOptions;
+  const { projectUuid } = window.app.pageOptions;
 
   return (
     <div className="sea-qa-portal-side-panel">
@@ -41,27 +43,17 @@ const SidePanel = ({ activePage, onPageChange, enableKB, isAnonymous }) => {
             onChange={onPrimaryTabChange}
           />
         </div>
-        <div className="sea-qa-portal-side-panel-actions">
-          {!isEditMode && isAnonymous && (
-            <div
-              className="sea-qa-portal-nav-item"
+        <div>
+          {isAnonymous ? (
+            <Button
+              color="outline-primary"
               onClick={() => { window.location.href = siteRoot + `portal/${projectUuid}/login/`; }}
-              title={gettext('Log in')}
+              title={gettext('Login')}
+              size="sm"
             >
-              <Icon symbol="support-portal" className="sea-qa-portal-nav-item-icon" />
-              <span className="sea-qa-portal-nav-item-name">{gettext('Log in')}</span>
-            </div>
-          )}
-          {!isEditMode && isExternalUser && !!username && (
-            <div
-              className="sea-qa-portal-nav-item"
-              onClick={() => { window.location.href = siteRoot + `portal-external/logout/${projectUuid}/`; }}
-              title={gettext('Log out')}
-            >
-              <Icon symbol="logout" className="sea-qa-portal-nav-item-icon" />
-              <span className="sea-qa-portal-nav-item-name">{gettext('Log out')}</span>
-            </div>
-          )}
+              {gettext('Login')}
+            </Button>
+          ) : <Account />}
         </div>
       </div>
     </div>
