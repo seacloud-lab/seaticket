e60c2342dbcdc0a446105b8f76545a7545668088
Jinlong Sun <kaichen999@outlook.com>
2026-05-14 16:01:49 +0800

    Update info.py

--- seahub/api2/endpoints/org_admin/info.py
+++ seahub/api2/endpoints/org_admin/info.py
@@ -87,11 +87,10 @@ class OrgAdminInfo(APIView):
         info['active_members'] = active_members
         info['role'] = org_role
         info['issues_usage'] = issues_usage
-        info['ai_monthly_credit'] = get_ai_credit_by_org_id(org_id)
-        info['additional_ai_credit'] = get_additional_credits_by_org_id(org_id)
-        info['ai_credit_limit'] = get_total_ai_credit_by_org_id(org_id)
+        info['ai_monthly_credit'] = round(get_ai_credit_by_org_id(org_id), 0)
+        info['additional_ai_credit'] = round(get_additional_credits_by_org_id(org_id), 0)
+        info['ai_credit_limit'] = round(get_total_ai_credit_by_org_id(org_id), 0)
         info['ai_credit_used'] = round(convert_cost_to_credit(get_ai_cost_by_org_id(org_id)), 0)
-
         return Response(info)
 
     def put(self, request):
