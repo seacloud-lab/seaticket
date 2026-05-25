7dfc47c127381b0c34ceaee4ff9b168ec3b812c7
JunhuiZhang <junhuizhang42@gmail.com>
2026-05-25 14:43:36 +0800

    fix: agent comment's author display

--- seahub/project/agent.py
+++ seahub/project/agent.py
@@ -568,6 +568,7 @@ class AgentActionConfirmView(APIView):
                 reply_content,
             )
             comment_id = result.get('id')
+            comment_author = result.get('author', '')
             comment_created_at = result.get('created_at', '')
             logger.info(f'Added reply comment #{comment_id} to GitHub issue {ctx["record_id"]}')
         except Exception as e:
@@ -595,7 +596,7 @@ class AgentActionConfirmView(APIView):
             comment_row = {
                 GithubIssueCommentsTable.comment_id.name: comment_id,
                 GithubIssueCommentsTable.issue_id.name: ctx['issue_id'],
-                GithubIssueCommentsTable.author.name: ctx['author'],
+                GithubIssueCommentsTable.author.name: comment_author,
                 GithubIssueCommentsTable.content.name: reply_content,
                 GithubIssueCommentsTable.created_time.name: comment_created_at,
                 GithubIssueCommentsTable.modified_time.name: comment_created_at,
