cac4f77261ae994f10b717b6b30b566dbc507be6
awu0403 <76416779+awu0403@users.noreply.github.com>
2026-06-24 16:22:01 +0800

    Update websocket-service.js

--- frontend/src/utils/websocket-service.js
+++ frontend/src/utils/websocket-service.js
@@ -1,9 +1,8 @@
-import { enableNotificationServer } from './../constants';
+import { enableNotificationServer, server } from './../constants';
 import projectAPI from '@/project/api/project-api';
 
 const getNotificationServerUrl = () => {
-  const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
-  return `${protocol}//${window.location.host}/ws`;
+  return `${server.replace(/^http/, 'ws')}/ws`;
 };
 
 class WebSocketClient {
