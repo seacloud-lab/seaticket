75b0b92c1de988737eb1f7c57293e7115571f3de
Michael An <1822852997@qq.com>
2026-06-25 11:08:34 +0800

    Delete emails success then unselect rows

--- frontend/src/project/main-panel/connections/view/connection/records/index.js
+++ frontend/src/project/main-panel/connections/view/connection/records/index.js
@@ -278,6 +278,7 @@ const Records = ({ projectUuid, permission, connectionID, toggleBar }) => {
       connectionsAPI.deleteConnectionEmail(projectUuid, connectionID, { thread_ids: threadIds })
     ).then(() => {
       context.eventBus.dispatch(SEA_METADATA_EVENT_BUS_TYPE.DELETE_ROWS, threadIds);
+      context.eventBus.dispatch(SEA_METADATA_EVENT_BUS_TYPE.SELECT_NONE);
       const successMessage = rows.length === 1
         ? gettext('Email thread has been moved to Trash.')
         : gettext('Email threads have been moved to Trash.');
