088e040837f900848b73c56631b25d525dd9a605
Michael An <2331806369@qq.com>
2026-07-23 18:31:50 +0800

    change group title and empty tip (#1615)

--- frontend/src/components/empty-tip/index.js
+++ frontend/src/components/empty-tip/index.js
@@ -9,7 +9,7 @@ const { mediaUrl } = window.app.config;
 const EmptyTip = ({ text, title, type, src, className, children }) => {
   return (
     <div className={classnames('empty-tip', className)}>
-      <img src={src || `${mediaUrl}img/no-items-tip.png`} alt="" width="88" height="88" className="no-items-img-tip" />
+      {src && <img src={src || `${mediaUrl}img/no-items-tip.png`} alt="" width="88" height="88" className="no-items-img-tip" />}
       {title && <span className="empty-tip-title">{title}</span>}
       {text && <span className="empty-tip-text" style={{ color: type === 'error' ? 'red' : '#666' }}>{text}</span>}
       {children}
