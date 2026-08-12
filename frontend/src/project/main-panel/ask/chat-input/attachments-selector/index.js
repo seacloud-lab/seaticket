bb05b5e612082f5c8ed82458731aeeb7dd70bc9c
Michael An <1822852997@qq.com>
2026-08-12 10:48:39 +0800

    close attachment selector ui when click item

--- frontend/src/project/main-panel/ask/chat-input/attachments-selector/index.js
+++ frontend/src/project/main-panel/ask/chat-input/attachments-selector/index.js
@@ -18,12 +18,11 @@ const AttachmentsSelector = ({
   onFileInputClick,
   canAddSources = true,
 }) => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isShowSelector, setIsShowSelector] = useState(false);
 
   const attachmentsRef = useRef([]);
-  const ref = useRef(null);
-
-  const [isShowSelector, setIsShowSelector] = useState(false);
+  const ref = useRef(null);  
 
   const openSelector = useCallback(() => {
     setIsShowSelector(true);
@@ -50,6 +49,8 @@ const AttachmentsSelector = ({
       if (key.startsWith('image')) return attachments.find(t => t.key === key);
       return attachmentsRef.current.find(t => t.key === key);
     }).filter(Boolean);
+    setIsDropdownOpen(false);
+    setIsShowSelector(false);
     propsOnChange && propsOnChange(newAttachments);
   }, [attachments, propsOnChange]);
 
@@ -58,7 +59,7 @@ const AttachmentsSelector = ({
 
   return (
     <>
-      <Dropdown isOpen={isOpen} direction="up" className="active-status-editor" toggle={() => setIsOpen(!isOpen)}>
+      <Dropdown isDropdownOpen={isDropdownOpen} direction="up" className="active-status-editor" toggle={() => setIsDropdownOpen(!isDropdownOpen)}>
         <DropdownToggle
           className="seaqa-ask-chat-attachments-selector seaqa-icon-btn border d-flex align-items-center"
           tag="div"
