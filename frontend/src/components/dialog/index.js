2c492b7ee49cd0dee87cab33bd62bb47e5dafa74
Michael An <1822852997@qq.com>
2026-08-21 15:11:42 +0800

    remove useless dialog

--- frontend/src/components/dialog/index.js
+++ frontend/src/components/dialog/index.js
@@ -1,13 +1,11 @@
 import CommonOperationConfirmationDialog from './common-operation-confirmation-dialog';
 import RestoreProjectDialog from './restore-project-dialog';
-import ClearTrashDialog from './clean-trash-dialog';
 import LinkVerifiedDialog from './link-verified-dialog';
 import TokenCreditUsedDetailDialog from './token-credit-used-detail-dialog';
 
 export {
   CommonOperationConfirmationDialog,
   RestoreProjectDialog,
-  ClearTrashDialog,
   LinkVerifiedDialog,
   TokenCreditUsedDetailDialog
 };
